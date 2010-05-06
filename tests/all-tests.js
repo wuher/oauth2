3c9e5fb7678519f98e61d4db9991c0e57db706cd
Janne Kuuskeri <janne.kuuskeri@gmail.com>
2010-05-06 13:48:31 +0300

    changed assertion types

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -21,14 +21,14 @@ exports.testConsumerBasic = function () {
     //             oauth.Consumer("yy", "xx");
     //         }, Error, "without new should raise error");
     var consumer = new oauth.Consumer("my-consumer-key", "my-consumer-secret");
-    assert.isEqual(consumer.key, "my-consumer-key", "key not set");
-    assert.isEqual(consumer.secret, "my-consumer-secret", "secret not set");
+    assert.isSame(consumer.key, "my-consumer-key", "key not set");
+    assert.isSame(consumer.secret, "my-consumer-secret", "secret not set");
 };
 
 
 exports.testConsumerToString = function () {
     var consumer = new oauth.Consumer("jedi", "sith");
-    assert.isEqual(consumer.toString(),
+    assert.isSame(consumer.toString(),
                        "oauth_consumer_key=jedi&oauth_consumer_secret=sith",
                        "not the same");
 };
@@ -44,41 +44,41 @@ exports.testTokenBasic = function () {
             new oauth.Token("my-secret")
         }, Error, "should raise error");
     var token = new oauth.Token("my-key", "my-secret");
-    assert.isEqual(token.key, "my-key", "key not set");
-    assert.isEqual(token.secret, "my-secret", "secret not set");
-    assert.isEqual(token.callback, null);
-    assert.isEqual(token.callback_confirmed, null);
-    assert.isEqual(token.verifier, null);
+    assert.isSame(token.key, "my-key", "key not set");
+    assert.isSame(token.secret, "my-secret", "secret not set");
+    assert.isSame(token.callback, null);
+    assert.isSame(token.callback_confirmed, null);
+    assert.isSame(token.verifier, null);
 };
 
 
 exports.testCallbackSetter = function () {
     var token = new oauth.Token("jedi", "sith");
     token.callback = "tatooine";
-    assert.isEqual(token.callback, "tatooine");
-    assert.isEqual(token.callback_confirmed, "true");
+    assert.isSame(token.callback, "tatooine");
+    assert.isSame(token.callback_confirmed, "true");
 };
 
 
 exports.testVerifierSetter = function () {
     var verifierset = {}, i, token = new oauth.Token("mara", "jade");
     token.verifier = "endor";
-    assert.isEqual(token.verifier, "endor");
+    assert.isSame(token.verifier, "endor");
     token.setVerifier("dantoiine");
-    assert.isEqual(token.verifier, "dantoiine");
+    assert.isSame(token.verifier, "dantoiine");
 
     // pretty naive, but better than nothing..
     for (i = 0; i < 100; i += 1) {
         token.verifier = null;
-        assert.notEqual(token.verifier, null);
+        assert.isDiff(token.verifier, null);
         assert.isTrue(!(token.verifier in verifierset));
-        assert.isEqual(token.verifier.length, 8);
+        assert.isSame(token.verifier.length, 8);
         verifierset[token.verifier] = true;
 
         token.setVerifier(null);
-        assert.notEqual(token.verifier, null);
+        assert.isDiff(token.verifier, null);
         assert.isTrue(!(token.verifier in verifierset));
-        assert.isEqual(token.verifier.length, 8);
+        assert.isSame(token.verifier.length, 8);
         verifierset[token.verifier] = true;
     }
 };
@@ -87,9 +87,12 @@ exports.testVerifierSetter = function () {
 exports.testCallbackGetter = function () {
     var token = new oauth.Token("yavin", "hoth");
     token.callback = "http://www.cs.tut.fi/~jedi/ready";
+    assert.isSame(token.getCallbackUrl(),
+                  "http://www.cs.tut.fi/~jedi/ready");
     token.setVerifier();
-    assert.isEqual(token.getCallbackUrl(),
-                   "http://www.cs.tut.fi/~jedi/ready?oauth_verifier=" + token.verifier);
+    assert.isSame(token.getCallbackUrl(),
+                  "http://www.cs.tut.fi/~jedi/ready?oauth_verifier=" + token.verifier);
+
 };
 
 
